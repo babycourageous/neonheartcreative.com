50c136eb3a66bde7189193c63e10b84a648fe269
Rene Dellefont <renedellefont@gmail.com>
2021-05-22 19:41:35 -0400

    refactor: add footer links

--- pages/index.tsx
+++ pages/index.tsx
@@ -101,6 +101,10 @@ export default function Home() {
 
       <footer tw="max-w-3xl flex items-center justify-center mt-6 mx-auto px-4 py-4">
         <div tw="p-1">
+          <a
+            href="https://www.instagram.com/neonheartcreative/"
+            target="_blank"
+          >
           <svg
             tw="w-6 h-6"
             xmlns="http://www.w3.org/2000/svg"
@@ -110,24 +114,23 @@ export default function Home() {
             <path d="M12,6a6,6,0,1,0,6,6A6,6,0,0,0,12,6Zm0,10a4,4,0,1,1,4-4A4,4,0,0,1,12,16Z"></path>
             <circle cx="18.5" cy="5.5" r="1.5"></circle>
           </svg>
+          </a>
         </div>
         <div tw="p-1">
+          <a href="mailto:neonheartcreative@gmail.com" target="_blank">
           <svg
             tw="w-6 h-6"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 512 512"
           >
-            <g>
-              <g>
                 <path
                   d="M467,61H45C20.218,61,0,81.196,0,106v300c0,24.72,20.128,45,45,45h422c24.72,0,45-20.128,45-45V106
 			C512,81.28,491.872,61,467,61z M460.786,91L256.954,294.833L51.359,91H460.786z M30,399.788V112.069l144.479,143.24L30,399.788z
 			 M51.213,421l144.57-144.57l50.657,50.222c5.864,5.814,15.327,5.795,21.167-0.046L317,277.213L460.787,421H51.213z M482,399.787
 			L338.213,256L482,112.212V399.787z"
                 />
-              </g>
-            </g>
           </svg>
+          </a>
         </div>
       </footer>
     </>

7fc2786ada9c345b38bf22fa6cea980cf6ec3fb3
Rene Dellefont <renedellefont@gmail.com>
2021-05-22 19:41:51 -0400

    refactor: responsice intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,23 +28,20 @@ export default function Home() {
       </header>
       <main tw="">
         <section tw="max-w-3xl sm:flex sm:flex-row-reverse mt-6 mx-auto px-4">
-          <p tw="text-3xl font-extralight">Hi! I'm Jess. </p>
-          <p>
-            I am a self taught interior designer based in Atlanta, GA that most
-            would say has been designing in some shape or form my whole life. I
-            tackle everything from office spaces to home Decor to DIY house
-            projects to special events.
-          </p>
-          <p tw="mt-2">
-            I know how to take your budget and bring your vision to life.
-          </p>
-          <div tw="flex items-center justify-center w-full mt-4">
-            <Image
-              src="/assets/images/hello-there.png"
-              alt=""
-              width={250}
-              height={250}
-            />
+          <div tw="sm:ml-6">
+            <p tw="text-3xl font-extralight">Hi! I'm Jess. </p>
+            <p>
+              I am a self taught interior designer based in Atlanta, GA that
+              most would say has been designing in some shape or form my whole
+              life. I tackle everything from office spaces to home Decor to DIY
+              house projects to special events.
+            </p>
+            <p tw="mt-2">
+              I know how to take your budget and bring your vision to life.
+            </p>
+          </div>
+          <div tw="flex items-center justify-center w-full mt-4 sm:mt-0">
+            <img src="/assets/images/hello-there.png" alt="" />
           </div>
         </section>
 
@@ -105,31 +102,31 @@ export default function Home() {
             href="https://www.instagram.com/neonheartcreative/"
             target="_blank"
           >
-          <svg
-            tw="w-6 h-6"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <path d="M20,0H4A4,4,0,0,0,0,4V20a4,4,0,0,0,4,4H20a4,4,0,0,0,4-4V4A4,4,0,0,0,20,0Zm2,20a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V4A2,2,0,0,1,4,2H20a2,2,0,0,1,2,2Z"></path>
-            <path d="M12,6a6,6,0,1,0,6,6A6,6,0,0,0,12,6Zm0,10a4,4,0,1,1,4-4A4,4,0,0,1,12,16Z"></path>
-            <circle cx="18.5" cy="5.5" r="1.5"></circle>
-          </svg>
+            <svg
+              tw="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M20,0H4A4,4,0,0,0,0,4V20a4,4,0,0,0,4,4H20a4,4,0,0,0,4-4V4A4,4,0,0,0,20,0Zm2,20a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V4A2,2,0,0,1,4,2H20a2,2,0,0,1,2,2Z"></path>
+              <path d="M12,6a6,6,0,1,0,6,6A6,6,0,0,0,12,6Zm0,10a4,4,0,1,1,4-4A4,4,0,0,1,12,16Z"></path>
+              <circle cx="18.5" cy="5.5" r="1.5"></circle>
+            </svg>
           </a>
         </div>
         <div tw="p-1">
           <a href="mailto:neonheartcreative@gmail.com" target="_blank">
-          <svg
-            tw="w-6 h-6"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-                <path
-                  d="M467,61H45C20.218,61,0,81.196,0,106v300c0,24.72,20.128,45,45,45h422c24.72,0,45-20.128,45-45V106
+            <svg
+              tw="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                d="M467,61H45C20.218,61,0,81.196,0,106v300c0,24.72,20.128,45,45,45h422c24.72,0,45-20.128,45-45V106
 			C512,81.28,491.872,61,467,61z M460.786,91L256.954,294.833L51.359,91H460.786z M30,399.788V112.069l144.479,143.24L30,399.788z
 			 M51.213,421l144.57-144.57l50.657,50.222c5.864,5.814,15.327,5.795,21.167-0.046L317,277.213L460.787,421H51.213z M482,399.787
 			L338.213,256L482,112.212V399.787z"
-                />
-          </svg>
+              />
+            </svg>
           </a>
         </div>
       </footer>

a87ee110ebdb5e7e8496fd54934a8a280ea7177b
Rene Dellefont <renedellefont@gmail.com>
2021-05-22 17:58:21 -0400

    feat: first draft design

---
+++ data/pricing.ts
@@ -0,0 +1,47 @@
+export const pricing = [
+  {
+    label: '45 minute introductory meeting',
+    description:
+      "..this is how it all begins. Let's get to know each other and your needs and decide together if Neon Heart is the right fit.",
+    price: 'FREE',
+  },
+  {
+    label: '30 minute Zoom Consultation',
+    description: 'For quickie design/decor consults',
+    price: '$40',
+  },
+  {
+    label: '1 hour in person Consultation',
+    description:
+      'For longer consultation. When you need someone to come in with opinions you can use. Lots of them.',
+    price: '$100',
+  },
+  {
+    label: '1 room design',
+    description:
+      'including digital mood board, spreadsheets of articles recommended',
+    price: '$500',
+  },
+  {
+    label: 'Kitchen design',
+    description:
+      'including digital mood board, spreadsheets of articles recommended',
+    price: '$1500',
+  },
+  {
+    label: 'Room Sketch-Up',
+    description:
+      'my take on what your space could look like from a photo you send me, including 1 revision',
+    price: '$50',
+  },
+  {
+    label: 'Hourly rate',
+    description: '(shopping, styling, etc.)',
+    price: '$75/hr',
+  },
+  {
+    label: 'Freelance Day Rate',
+    description: '8 hours shopping/assembly for Clients',
+    price: '$375',
+  },
+]
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
-import { GlobalStyles } from 'twin.macro'
+import tw, { GlobalStyles } from 'twin.macro'
 import type { AppProps } from 'next/app'
+import { Global, css } from '@emotion/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
+      <Global
+        styles={css`
+          body {
+            ${tw`text-gray-800`}
+          }
+        `}
+      />
       <Component {...pageProps} />
     </>
   )

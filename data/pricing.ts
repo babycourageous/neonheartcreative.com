6cff7455cad8082c6bd8bd9516239fb5c2ea7295
Rene Dellefont <renedellefont@gmail.com>
2023-02-17 16:26:26 -0500

    update pricing

--- data/pricing.ts
+++ data/pricing.ts
@@ -1,6 +1,6 @@
 export const pricing = [
   {
-    label: '45 minute introductory meeting',
+    label: '30 minute introductory meeting',
     description:
       "This is how it all begins. Let's get to know each other and your needs and decide together if Neon Heart is the right fit.",
     price: 'FREE',
@@ -8,40 +8,40 @@ export const pricing = [
   {
     label: '30 minute Zoom Consultation',
     description: 'For quickie design/decor consults',
-    price: '$50',
+    price: '$100',
   },
   {
     label: '1 hour in person Consultation',
     description:
       'For longer consultation. When you need someone to come in with opinions you can use. Lots of them.',
-    price: '$150',
+    price: '$250',
   },
   {
     label: '1 room design',
     description:
       'Including digital mood board, spreadsheets of articles recommended',
-    price: '$550',
+    price: '$650',
   },
   {
     label: 'Kitchen design',
     description:
       'Including digital mood board, spreadsheets of articles recommended',
-    price: '$600-1500',
+    price: '$1000-2500',
   },
   {
     label: 'Hourly rate',
     description: '(shopping, styling, etc.)',
-    price: '$75/hr',
+    price: '$100/hr',
   },
   {
     label: 'Freelance Day Rate',
     description: '8 hours shopping/assembly/styling for Clients',
-    price: '$450',
+    price: '$550',
   },
   {
     label: 'New Home Overview',
     description:
       'Consultation and detailed spreadsheet on everything you need for your new home to be move-in ready. Includes paint colors, light fixtures, hardware, and suggestions on updates you could make.',
-    price: '$500',
+    price: '$600',
   },
 ]

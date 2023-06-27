2e24ac7265bfddfa92a113bd18ed5d40c657aee0
Rene Dellefont <renedellefont@gmail.com>
2023-06-27 13:53:00 -0400

    chore: next image codemod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from "next/legacy/image"
+import Image from "next/image"
 
 import { pricing } from '../data/pricing'
 import SubscribeForm from '../components/subscribe-form'
@@ -21,8 +21,9 @@ export default function Home() {
           <Image
             src="/assets/images/nh-logo.png"
             alt=""
-            layout="fill"
-            objectFit="contain"
+            fill
+            sizes="100vw"
+            className="object-contain"
           />
         </div>
       </header>
@@ -168,8 +169,9 @@ export default function Home() {
             <Image
               src="/assets/images/newsletter-heading.png"
               alt=""
-              layout="fill"
-              objectFit="contain"
+              fill
+              sizes="100vw"
+              className="object-contain"
             />
           </div>
           <div className="mt-4">
@@ -204,9 +206,9 @@ export default function Home() {
             >
               <path
                 d="M467,61H45C20.218,61,0,81.196,0,106v300c0,24.72,20.128,45,45,45h422c24.72,0,45-20.128,45-45V106
-			C512,81.28,491.872,61,467,61z M460.786,91L256.954,294.833L51.359,91H460.786z M30,399.788V112.069l144.479,143.24L30,399.788z
-			 M51.213,421l144.57-144.57l50.657,50.222c5.864,5.814,15.327,5.795,21.167-0.046L317,277.213L460.787,421H51.213z M482,399.787
-			L338.213,256L482,112.212V399.787z"
+      C512,81.28,491.872,61,467,61z M460.786,91L256.954,294.833L51.359,91H460.786z M30,399.788V112.069l144.479,143.24L30,399.788z
+       M51.213,421l144.57-144.57l50.657,50.222c5.864,5.814,15.327,5.795,21.167-0.046L317,277.213L460.787,421H51.213z M482,399.787
+      L338.213,256L482,112.212V399.787z"
               />
             </svg>
           </a>

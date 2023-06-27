709537a4fbe0c112d5e1ce627965bc087a35b045
Rene Dellefont <renedellefont@gmail.com>
2023-06-27 13:47:58 -0400

    chore: next image legacy codemod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Image from "next/legacy/image"
 
 import { pricing } from '../data/pricing'
 import SubscribeForm from '../components/subscribe-form'
